cc20ce8d623f6274494cfbe51b6dcfed9231cb3a
Antonio Correia <antonio.alves.correia@gmail.com>
2021-11-26 13:53:36 -0300

    adding references to UI project and fixing problems to generate swagger file

--- templates/SparcTemplate.Web/Program.cs
+++ templates/SparcTemplate.Web/Program.cs
@@ -6,6 +6,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<IConfiguration>(_ => builder.Configuration);
+
+//await builder.Services.AddSelfHostedApi<SparcTemplateApi>(
+//                    "MyContactNetwork API",
+//                    builder.Configuration["ApiUrl"],
+//                    "Web");
+
 
 await builder.Build().RunAsync();
