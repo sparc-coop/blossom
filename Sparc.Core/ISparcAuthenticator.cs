a1369da217c1fbc1f45421a5b8a5560c3c1b01fc
Antonio Correia <antonio.alves.correia@gmail.com>
2021-12-03 19:54:18 -0300

    Adding AzureADB2C extensions to MAUI

--- Sparc.Core/ISparcAuthenticator.cs
+++ Sparc.Core/ISparcAuthenticator.cs
@@ -6,6 +6,7 @@ namespace Sparc.Core
     public interface ISparcAuthenticator
     {
         public Task<bool> LoginAsync();
+        public Task<ClaimsPrincipal> LoginAsync(string returnUrl);
         public Task LogoutAsync();
         public ClaimsPrincipal User { get; set; }
     }
