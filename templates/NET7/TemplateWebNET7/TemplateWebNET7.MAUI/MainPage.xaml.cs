a4ebed09052c04c44aafde6c4fb5d72539be9858
Antonio Correia <antonio.alves.correia@gmail.com>
2022-12-01 01:32:14 -0300

    removing legacy projects from solution

---
+++ templates/NET7/TemplateWebNET7/TemplateWebNET7.MAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TemplateWebNET7.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ templates/NET7/TemplateWebNET7/TemplateWebNET7.MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TemplateWebNET7.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
