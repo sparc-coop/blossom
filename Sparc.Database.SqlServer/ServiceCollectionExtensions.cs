999f2a4472814f7dec0b8f3369030ce1b16581b8
Joseph Young <joseph@josephayoung.com>
2022-12-02 08:39:27 -0600

    Add service lifetime option to SqlServer

--- Sparc.Database.SqlServer/ServiceCollectionExtensions.cs
+++ Sparc.Database.SqlServer/ServiceCollectionExtensions.cs
@@ -6,18 +6,18 @@ namespace Sparc.Database.SqlServer;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddSqlServer<T>(this IServiceCollection services, string connectionString) where T : DbContext
+    public static IServiceCollection AddSqlServer<T>(this IServiceCollection services, string connectionString, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) where T : DbContext
     {
-        services.AddSqlServerWithoutRepository<T>(connectionString);
-        services.AddScoped(typeof(DbContext), typeof(T));
-        services.AddScoped(typeof(IRepository<>), typeof(SqlServerRepository<>));
-        services.AddScoped(typeof(ISqlRepository<>), typeof(SqlServerRepository<>));
+        services.AddSqlServerWithoutRepository<T>(connectionString, serviceLifetime);
+        services.Add(new ServiceDescriptor(typeof(DbContext), typeof(T), serviceLifetime));
+        services.Add(new ServiceDescriptor(typeof(IRepository<>), typeof(SqlServerRepository<>), serviceLifetime));
+        services.Add(new ServiceDescriptor(typeof(ISqlRepository<>), typeof(SqlServerRepository<>), serviceLifetime));
         return services;
     }
 
-    public static IServiceCollection AddSqlServerWithoutRepository<T>(this IServiceCollection services, string connectionString) where T : DbContext
+    public static IServiceCollection AddSqlServerWithoutRepository<T>(this IServiceCollection services, string connectionString, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) where T : DbContext
     {
-        services.AddDbContext<T>(options => options.UseSqlServer(connectionString));
+        services.AddDbContext<T>(options => options.UseSqlServer(connectionString), serviceLifetime);
         return services;
     }
 }
