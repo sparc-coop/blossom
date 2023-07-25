b78e323fe45adc83aa61d582b03275a79d090217
Christine Antonio <christinejm.antonio@gmail.com>
2023-07-25 14:03:23 -0700

    implement OnAfterRenderAsync() to fix matterjs canvas rendering issue; delete old files

--- Sparc.Blossom.Client.Example/wwwroot/game-example.js
+++ Sparc.Blossom.Client.Example/wwwroot/game-example.js
@@ -1,254 +1,133 @@
-﻿let Engine = Matter.Engine;
-let Render = Matter.Render;
-let Events = Matter.Events;
-let Runner = Matter.Runner;
-let Bodies = Matter.Bodies;
-let Body = Matter.Body;
-let Composite = Matter.Composite;
-let World = Matter.World;
-let Composites = Matter.Composites;
-let MouseConstraint = Matter.MouseConstraint;
-let Mouse = Matter.Mouse;
-let Vertices = Matter.Vertices;
-let Svg = Matter.Svg;
+﻿function createGame() {
 
-//window.canvasInterop = {
-//    setupCanvas: function () {
-//        var canvas = document.getElementById("canvas--game-example");
-//        var context = canvas.getContext("2d");
-//        // Perform various operations on the canvas using the context object
-//        // Example: Draw a rectangle
-//        //context.fillStyle = "red";
-//        //context.fillRect(10, 10, 100, 100);
-//    }
-//};
+    var canvas = document.getElementById("game-container");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
+    window.addEventListener("resize", function () {
+        document.getElementById("game-container").width = window.innerWidth;
+        document.getElementById("game-container").height = window.innerHeight;
+    });
 
-class MatterRectCirc {
-    constructor(element) {
-        this.element = document.querySelector(element);
-        this.width = this.element.getBoundingClientRect().width;
-        this.height = document.querySelector(".js-banner-blue").getBoundingClientRect().height;
-
-        // create an engine
-        this.engine = Engine.create();
-        this.engine.gravity.y = 1;
-        this.world = this.engine.world;
-
-        // create a renderer
-        this.render = Render.create({
-            element: this.element,
-            engine: this.engine,
-            options: {
-                width: this.width,
-                height: this.height,
-
-                wireframes: false,
-                background: "transparent",
-            },
-        });
-        this.addWalls();
-        this.addBodies();
-        this.updateBodies();
-        this.setupMouse();
-
-        // run the renderer
-        Render.run(this.render);
-
-        // create runner
-        this.runner = Runner.create();
-
-        // run the engine
-        Runner.run(this.runner, this.engine);
-        console.log("ran engine");
+    function percentX(percent) {
+        return Math.round((percent / 100) * canvas.width);
     }
 
-    addWalls() {
-        console.log("add walls");
-
-        let roof = Bodies.rectangle(0, 0 - 75 * 2, this.width * 2, 60 * 5, {
-            isStatic: true,
-            label: "wall",
-            render: {
-                fillStyle: "transparent",
-                strokeStyle: "transparent",
-            },
-        });
-        let ground = Bodies.rectangle(0, this.height + 25, this.width * 2, 60, {
-            isStatic: true,
-            label: "wall",
-            render: {
-                fillStyle: "transparent",
-                strokeStyle: "transparent",
-            },
-        });
-        let leftWall = Bodies.rectangle(0 - 75 * 2, 0, 60 * 5, this.height * 2, {
-            isStatic: true,
-            label: "wall",
-            render: {
-                fillStyle: "transparent",
-                strokeStyle: "transparent",
-            },
-        });
-        let rightWall = Bodies.rectangle(this.width + 74 * 2, 0, 60 * 5, this.height * 2, {
-            isStatic: true,
-            label: "wall",
-            render: {
-                fillStyle: "transparent",
-                strokeStyle: "transparent",
-            },
-        }
-        );
-
-        Composite.add(this.world, [ground, roof, leftWall, rightWall]);
+    function percentY(percent) {
+        return Math.round((percent / 100) * canvas.height);
     }
 
-    addBodies() {
-        console.log("add bodies");
+    var Engine = Matter.Engine,
+        Render = Matter.Render,
+        Runner = Matter.Runner,
+        Bodies = Matter.Bodies,
+        Svg = Matter.Svg,
+        Vertices = Matter.Vertices,
+        Mouse = Matter.Mouse,
+        MouseConstraint = Matter.MouseConstraint,
+        World = Matter.World;
 
-        let borderRadius;
-        const reduceSize = 8;
-        let rectSize;
-        let circleSize;
-        let gap;
+    // create an engine
+    const engine = Engine.create();
+    world = engine.world;
 
-        if (window.innerWidth <= 375) {
-            rectSize = Math.round(this.height / 2.5);
-            circleSize = Math.round(this.height / 5.5);
-            borderRadius = 24;
-            gap = 0;
-        } else {
-            rectSize = Math.round((this.height / 2) - reduceSize);
-            circleSize = Math.round(this.height / 4);
-            borderRadius = 32;
-            gap = 32;
+    // create runner
+    const runner = Runner.create();
+    Runner.run(runner, engine);
+
+    // gravity
+    world.gravity.y = 0, 000001;
+
+    // create a renderer
+    const render = Render.create({
+        element: canvas,
+        engine: engine,
+        options: {
+            wireframes: false,
+            showInternalEdges: false,
+            width: percentX(100),
+            height: percentY(100),
+            background: "transparent",
         }
+    });
 
-        let positionStart;
-        let positionBottom;
-        let positionTop;
+    Render.run(render);
 
-        function leftColumnPosition(prop) {
-            if (prop.body === "rectangle") {
-                if (prop.coord === "x") return positionStart = Math.round(rectSize / 2) + gap;
-                if (prop.coord === "y") return positionBottom = (positionStart + rectSize) - gap;
-            }
+    // boundaries
 
-            if (prop.body === "circle") {
-                if (prop.coord === "x") return positionStart = circleSize + gap;
-                if (prop.coord === "y") return positionTop = circleSize;
-            }
-        }
+    const bodies = []
 
-        function rightColumnPosition(prop) {
-            if (prop.body === "rectangle") {
-                if (prop.coord === "x") return positionStart = rectSize + circleSize + gap;
-                if (prop.coord === "y") return positionTop = (rectSize / 2);
-            }
+    var ceiling = Bodies.rectangle(percentX(100) / 2, percentY(0) - 10, percentX(100), 20, { isStatic: true });
+    var floor = Bodies.rectangle(percentX(100) / 2, percentY(100) + 10, percentX(100), 20, { isStatic: true });
+    var rightWall = Bodies.rectangle(percentX(100) + 10, percentY(100) / 2, 20, percentY(100), { isStatic: true });
+    var leftWall = Bodies.rectangle(percentX(0) - 10, percentY(100) / 2, 20, percentY(100), { isStatic: true });
+    ceiling.render.visible = false;
+    floor.render.visible = false;
+    rightWall.render.visible = false;
+    leftWall.render.visible = false;
+    bodies.push(ceiling);
+    bodies.push(floor);
+    bodies.push(rightWall);
+    bodies.push(leftWall);
 
-            if (prop.body === "circle") {
-                if (prop.coord === "x") return positionStart = circleSize + rectSize + gap;
-                if (prop.coord === "y") return positionBottom = circleSize + rectSize;
-            }
-        }
+    // create bodies from shapes and add to world
 
-        const boxBottom = Bodies.rectangle(
-            leftColumnPosition({ body: "rectangle", coord: "x" }),
-            leftColumnPosition({ body: "rectangle", coord: "y" }),
-            rectSize,
-            rectSize,
-            {
-                isStatic: true,
-                chamfer: { radius: borderRadius },
-                render: {
-                    fillStyle: "#19A97B",
-                    strokeStyle: "transparent",
-                },
-            }
-        );
-        const circleTop = Bodies.circle(
-            leftColumnPosition({ body: "circle", coord: "x" }),
-            leftColumnPosition({ body: "circle", coord: "y" }),
-            circleSize,
-            {
-                isStatic: true,
-                render: {
-                    fillStyle: "#19A97B",
-                    strokeStyle: "transparent",
-                },
-            }
-        );
-        const circleBottom = Bodies.circle(
-            rightColumnPosition({ body: "circle", coord: "x" }),
-            rightColumnPosition({ body: "circle", coord: "y" }),
-            circleSize,
-            {
-                isStatic: true,
-                render: {
-                    fillStyle: "#19A97B",
-                    strokeStyle: "transparent",
-                },
-            }
-        );
-        const boxTop = Bodies.rectangle(
-            rightColumnPosition({ body: "rectangle", coord: "x" }),
-            rightColumnPosition({ body: "rectangle", coord: "y" }),
-            rectSize,
-            rectSize,
-            {
-                isStatic: true,
-                chamfer: { radius: borderRadius },
-                render: {
-                    fillStyle: "#19A97B",
-                    strokeStyle: "transparent",
-                },
-            }
-        );
-        Composite.add(this.world, [boxBottom, circleTop, circleBottom, boxTop]);
-    }
+    const square = document.getElementById("square"),
+        triangle = document.getElementById("triangle"),
+        circle = document.getElementById("circle"),
+        diamond = document.getElementById("diamond");
 
-    updateBodies() {
-        console.log("update bodies");
+    var squareV,
+        triangleV,
+        circleV,
+        diamondV;
 
-        ScrollTrigger.create({
-            trigger: ".matter-rect-circ",
-            start: "center bottom",
-            once: true,
-            // markers: true,
-            onEnter: () => {
-                const allBodies = Matter.Composite.allBodies(this.engine.world);
-                allBodies.forEach((body) => {
-                    if (body.label !== "wall") {
-                        Body.setStatic(body, false);
-                    }
-                });
-            },
-        });
-    }
+    const squarePath = document.getElementById("squarePath");
+    squareV = Bodies.fromVertices(
+        134,
+        134,
+        Svg.pathToVertices(squarePath),
+        true
+    );
 
-    setupMouse() {
-        console.log("setup mouse");
+    const trianglePath = document.getElementById("trianglePath");
+    triangleV = Bodies.fromVertices(
+        134,
+        134,
+        Svg.pathToVertices(trianglePath),
+        true
+    );
 
-        if (window.innerWidth < 1024) return;
+    const circlePath = document.getElementById("circlePath");
+    circleV = Bodies.fromVertices(
+        134,
+        134,
+        Svg.pathToVertices(circlePath),
+        true
+    );
 
-        const mouse = Mouse.create(this.render.canvas);
-        const mouseConstraint = MouseConstraint.create(this.engine, {
+    const diamondPath = document.getElementById("diamondPath");
+    diamondV = Bodies.fromVertices(
+        134,
+        134,
+        Svg.pathToVertices(diamondPath),
+        true
+    );
+
+    bodies.push(squareV, triangleV, circleV, diamondV);
+    World.add(world, bodies);
+
+    // mouse control
+    let mouse = Mouse.create(render.canvas),
+        mouseConstraint = MouseConstraint.create(engine, {
             mouse: mouse,
             constraint: {
-                stiffness: 0.1,
+                stiffness: 0.2,
                 render: {
-                    visible: false,
-                },
-            },
+                    visible: false
+                }
+            }
         });
 
-        Composite.add(this.world, mouseConstraint);
-
-        // keep the mouse in sync with rendering
-        this.render.mouse = mouse;
-
-        // Allow page scrolling in matter.js window
-        mouse.element.removeEventListener("mousewheel", mouse.mousewheel);
-        mouse.element.removeEventListener("DOMMouseScroll", mouse.mousewheel);
-    }
+    World.add(world, mouseConstraint);
 }
