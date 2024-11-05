a519f5a8ab7a8e8cc3a8a340708069fb126c2181
Christine Antonio <christinejm.antonio@gmail.com>
2024-11-05 14:42:39 -0800

    Revert "add LoginStates to Blossom Authenticator, update references in KoriLogin"
    This reverts commit a737f6005904c3663c80388ef056f9d88ba4e088.

--- Sparc.Blossom.Core/Authentication/IAuthenticator.cs
+++ Sparc.Blossom.Core/Authentication/IAuthenticator.cs
@@ -8,6 +8,4 @@ public interface IAuthenticator
     public Task<ClaimsPrincipal> LoginAsync(string returnUrl);
     public Task LogoutAsync();
     public ClaimsPrincipal User { get; set; }
-    LoginStates LoginState { get; set; }
-    public string? Message { get; set; }
 }
