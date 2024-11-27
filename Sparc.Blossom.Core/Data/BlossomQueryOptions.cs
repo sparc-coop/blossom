83a307c09bc5c3f15b98ec233b4cd48a214fe070
Joseph Young <joseph@josephayoung.com>
2024-11-27 09:52:49 -0600

    Add additional CRUD options

--- Sparc.Blossom.Core/Data/BlossomQueryOptions.cs
+++ Sparc.Blossom.Core/Data/BlossomQueryOptions.cs
@@ -1,9 +1,10 @@
 ﻿namespace Sparc.Blossom.Api;
 
-public class BlossomQueryOptions(int skip = 0, int? take = null)
+public class BlossomQueryOptions(int skip = 0, int? take = null, string? searchTerm = null)
 {
     public int Skip { get; } = skip;
     public int? Take { get; } = take;
+    public string? SearchTerm { get; } = searchTerm;
 }
 
 public class BlossomQueryResult<T>(IEnumerable<T> items, int totalCount)
