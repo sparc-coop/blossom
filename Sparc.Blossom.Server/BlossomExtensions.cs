99e9da552aeb392c0affffcc9dbd2a1becda8852
Joseph Young <joseph@josephayoung.com>
2024-12-30 08:14:49 -0600

    Move Dexie to Sparc.Blossom

---
+++ Sparc.Blossom.Server/BlossomExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.JSInterop;
+
+namespace Sparc.Blossom;
+
+public static class BlossomExtensions
+{
+    public static Task<List<T>> ToListAsync<T>(this IQueryable<T> queryable)
+    {
+        return EntityFrameworkQueryableExtensions.ToListAsync(queryable);
+    }
+
+    public static Lazy<Task<IJSObjectReference>> Import(this IJSRuntime js, string module)
+    {
+        return new(() => js.InvokeAsync<IJSObjectReference>("import", module).AsTask());
+    }
+}
