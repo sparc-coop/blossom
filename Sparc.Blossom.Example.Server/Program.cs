138a3fba8ae0f0eb1f0e061f3012b31a1b6bbddf
Joseph Young <joseph@josephayoung.com>
2024-12-31 10:32:58 -0600

    Disable prerendering & pre-population of repository, and generation to disk

--- Sparc.Blossom.Example.Server/Program.cs
+++ Sparc.Blossom.Example.Server/Program.cs
@@ -5,9 +5,9 @@ using TodoItems;
 var builder = BlossomApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var items = app.Services.CreateScope().ServiceProvider.GetRequiredService<IRepository<TodoItem>>();
-await items.AddAsync(new TodoItem("First", "First one"));
-await items.AddAsync(new TodoItem("Second", "Second one"));
-await items.AddAsync(new TodoItem("Third", "Third one"));
+//var items = app.Services.CreateScope().ServiceProvider.GetRequiredService<IRepository<TodoItem>>();
+//await items.AddAsync(new TodoItem("First", "First one"));
+//await items.AddAsync(new TodoItem("Second", "Second one"));
+//await items.AddAsync(new TodoItem("Third", "Third one"));
 
 await app.RunAsync<App>();
