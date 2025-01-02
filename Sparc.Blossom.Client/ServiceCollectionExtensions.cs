c6d7180828195773674c952c51633198f33c13bf
Joseph Young <joseph@josephayoung.com>
2025-01-02 10:59:31 -0600

    Simplify and setup API endpoints in Sparc.Kori

--- Sparc.Blossom.Client/ServiceCollectionExtensions.cs
+++ Sparc.Blossom.Client/ServiceCollectionExtensions.cs
@@ -6,12 +6,18 @@ namespace Sparc.Blossom;
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddBlossomApi<T>(this IServiceCollection services, string baseUrl)
+        => services.AddBlossomApi<T>(new Uri(baseUrl));
+
+    public static IServiceCollection AddBlossomApi<T>(this IServiceCollection services, Uri baseUri)
     {
         services.AddRefitClient<IBlossomHttpClient<T>>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(baseUrl));
+            .ConfigureHttpClient(c => c.BaseAddress = baseUri);
 
         services.AddTransient<IRunner<T>, BlossomHttpClientRunner<T>>();
-        
+
         return services;
     }
+
+    public static IServiceCollection AddBlossomApi<T>(this IServiceCollection services, Uri baseUri, string path)
+        => services.AddBlossomApi<T>(new Uri(baseUri, path));
 }
