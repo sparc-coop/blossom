1aa4b1a7d3d57dfbb29cc0325072371f11ccdbc2
Joseph Young <joseph@josephayoung.com>
2025-01-14 08:58:16 -0600

    Add missing client endpoint mappings

--- Sparc.Blossom/Platforms/Server/BlossomServerApplication.cs
+++ Sparc.Blossom/Platforms/Server/BlossomServerApplication.cs
@@ -114,9 +114,11 @@ public class BlossomServerApplication : IBlossomApplication
         var group = Host.MapGroup(baseUrl);
         group.MapGet("{id}", async (IRunner<T> runner, string id) => await runner.Get(id));
         group.MapPost("", async (IRunner<T> runner, object[] parameters) => await runner.Create(parameters));
+        group.MapPost("_queries/{name}", async (IRunner<T> runner, string name, object[] parameters) => await runner.ExecuteQuery<object?>(name, parameters));
         group.MapPost("_undo", async (IRunner<T> runner, string id, long? revision) => await runner.Undo(id, revision));
         group.MapPost("_redo", async (IRunner<T> runner, string id, long? revision) => await runner.Redo(id, revision));
         group.MapGet("_metadata", async (IRunner<T> runner) => await runner.Metadata());
+        group.MapPut("{id}/{name}", async (IRunner<T> runner, string id, string name, object[] parameters) => await runner.Execute(id, name, parameters));
         group.MapPatch("{id}", async (IRunner<T> runner, string id, BlossomPatch patch) => await runner.Patch(id, patch));
         //group.MapPost("_queries", async (IRunner<T> runner, string name, BlossomQueryOptions options, object[] parameters) => await runner.ExecuteQuery(name, options, parameters));
         group.MapDelete("{id}", async (IRunner<T> runner, string id) => await runner.Delete(id));
