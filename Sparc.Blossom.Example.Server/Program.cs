557573b7bb2a47ca45e7332f8dd03b4274f64e55
Joseph Young <joseph@josephayoung.com>
2025-03-06 08:24:05 -0600

    Created PouchRepository in Kori

--- Sparc.Blossom.Example.Server/Program.cs
+++ Sparc.Blossom.Example.Server/Program.cs
@@ -1,9 +1,11 @@
 using Sparc.Blossom.Example.Single;
 using TodoItems;
+using Sparc.Kori;
 
 var builder = BlossomApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IRandomStringGenerator, RandomStringGenerator>();
+builder.Services.AddKoriDb();
 
 var app = builder.Build();
 
