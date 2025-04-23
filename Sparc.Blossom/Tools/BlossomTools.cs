8361a08ea6268498be62dc116541a51d32b6a249
Joseph Young <joseph@josephayoung.com>
2025-04-23 10:55:05 -0500

    Add Refitter and Cloud publish settings

--- Sparc.Blossom/Tools/BlossomTools.cs
+++ Sparc.Blossom/Tools/BlossomTools.cs
@@ -5,7 +5,7 @@ public class BlossomTools
     public static string FriendlyId()
     {
         var client = new HttpClient();
-        var webRequest = new HttpRequestMessage(HttpMethod.Get, "http://sparc-blossom-tools.azurewebsites.net/friendlyid");
+        var webRequest = new HttpRequestMessage(HttpMethod.Get, "https://blossom-cloud.azurewebsites.net/tools/friendlyid");
         var response = client.Send(webRequest);
         if (!response.IsSuccessStatusCode)
             return Guid.NewGuid().ToString();
