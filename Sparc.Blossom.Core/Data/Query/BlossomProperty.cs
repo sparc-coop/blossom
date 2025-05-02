d3fc48d3ebf1e2b3e05e48c25a55d2b349500182
Joseph Young <joseph@josephayoung.com>
2025-05-02 09:03:44 -0500

    Refactor components to use Sparc.Blossom library
    Updated Razor files to replace Blossom components with Sparc.Blossom. Removed UpdatePolicies.razor and added new pages for data exploration. Enhanced UI in BlossomEntity and BlossomExplorer, and improved styling with new CSS/SCSS files. Updated grid features and search functionality in BlossomList. Enhanced property handling in BlossomProperty and related files. Overall, these changes improve functionality and user experience across the application.

--- Sparc.Blossom.Core/Data/Query/BlossomProperty.cs
+++ Sparc.Blossom.Core/Data/Query/BlossomProperty.cs
@@ -52,6 +52,7 @@ public class BlossomProperty
     public decimal? DistinctPercentage => TotalCount == 0 ? 0 : DistinctValues / (decimal?)TotalCount;
     public Dictionary<string, dynamic> AvailableValues { get; set; } = [];
     public List<T> GetAvailableValues<T>() => AvailableValues.Values.Cast<T>().ToList();
+    public List<object> GetAvailableValues(Type type) => AvailableValues.Values.Select(x => Convert.ChangeType((object)x, type)).ToList();
 
     public object? _value;
     public object? Value(object entity) => _value ?? (CanRead ? Property?.GetValue(entity) : null);
