8cb966c7880382e7a0687409ca84fbab34017d63
Christine Antonio <christinejm.antonio@gmail.com>
2025-07-10 15:00:30 -0700

    domain card component for domain list, domain page, add favicon

--- Sparc.Core/Authentication/SparcDomain.cs
+++ Sparc.Core/Authentication/SparcDomain.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using Bogus;
 using Sparc.Blossom;
 
 namespace Sparc.Engine;
@@ -7,6 +7,18 @@ public class SparcDomain(string domain) : BlossomEntity<string>(Guid.NewGuid().T
 {
     public string Domain { get; set; } = domain;
     public List<SparcProduct> Products { get; set; } = [];
+    public int TotalUsage = new Random().Next(1, 100000);
+    //public int TotalUsage => Products.Sum(p => p.UsageMeter);
 
     public bool HasProduct(string policyName) => policyName == "Auth" || Products.Any(p => p.ProductId == policyName);
+
+    public static IEnumerable<SparcDomain> Generate(int qty)
+    {
+        var faker = new Faker<SparcDomain>()
+            .CustomInstantiator(f => new SparcDomain(
+                f.Lorem.Sentence(3)
+            ));
+
+        return faker.Generate(qty);
+    }
 }
