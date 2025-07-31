5cbcbc6322dc4eb7a38453a42960f0146d7c13be
Joseph Young <joseph@josephayoung.com>
2025-07-31 10:02:25 -0500

    Initial presence get/set logic

--- Sparc.Blossom/SparcEngine/Chat/ISparcChat.cs
+++ Sparc.Blossom/SparcEngine/Chat/ISparcChat.cs
@@ -1,29 +1,36 @@
 ﻿using Refit;
-
+using Sparc.Blossom.Authentication;
 using Sparc.Core.Chat;
 
 namespace Sparc.Engine.Chat;
 
 public interface ISparcChat
 {
-    [Get("/chat/getrooms")]
+    [Get("/_amtrix/celint/v3/users/{}/status")]
+    Task<MatrixPresence> GetUserAsync();
+    
+    [Get("/_matrix/client/v3/publicRooms")]
     Task<List<Room>> GetRoomsAsync();
 
-    [Post("/chat/rooms/create")]
-    Task<Room> CreateRoomAsync(Room room);
+    [Post("/_matrix/client/v3/createRoom")]
+    Task<Room> CreateRoomAsync(CreateRoomRequest request);
 
-    [Post("/chat/rooms/join")]
+    [Post("/_matrix/client/v3/join/{roomId}")]
     Task<Room> JoinRoomAsync(string roomId);
 
-    [Post("/chat/rooms/{roomId}/join")]
+    [Post("/_matrix/client/v3/rooms/{roomId}/leave")]
     Task<Room> LeaveRoomAsync(string roomId);
 
-    [Post("/chat/rooms/{roomId}/leave")]
-    Task<Room> InviteRoomAsync(string roomId, string userId);
+    [Post("/_matrix/client/v3/rooms/{roomId}/invite")]
+    Task<Room> InviteToRoomAsync(string roomId, InviteToRoomRequest request);
 
-    [Get("/chat/rooms/{roomId}/messages")]
+    [Get("/_matrix/client/v3/rooms/{roomId}/messages")]
     Task<List<MessageEvent>> GetMessagesAsync(string roomId);
 
-    [Post("/chat/rooms/sendmessage")]
-    Task<Event> SendMessageAsync(MessageEvent message);
+    [Post("/_matrix/client/v3/rooms/{roomId}/send/{eventType}/{txnId}")]
+    Task<Event> SendMessageAsync(string roomId, string eventType, string txnId, SendMessageRequest request);
 }
+
+public record InviteToRoomRequest(string UserId);
+public record CreateRoomRequest(string Name, bool IsDirect, string Visibility = "private", List<string>? Invite = null);
+public record SendMessageRequest(string Body, string MsgType = "m.text");
