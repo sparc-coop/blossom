dd1dbc3fd62b04f4f50ddf33144a69cd3f0ef29f
Joseph Young <joseph@josephayoung.com>
2025-08-01 09:22:15 -0500

    Progress

---
+++ Sparc.Core/Chat/MatrixMessage.cs
@@ -0,0 +1,25 @@
+﻿using Sparc.Engine;
+
+namespace Sparc.Core.Chat;
+
+public class MatrixMessage
+{
+    public string MsgType { get; set; }
+    public string Body { get; set; }
+}
+
+public class MatrixMessageEvent : MatrixEvent<MatrixMessage>
+{
+    public MatrixMessageEvent(string roomId, string sender, MatrixMessage content, List<MatrixEvent>? previousEvents = null)
+        : base("m.room.message", roomId, sender, content, previousEvents)
+    {
+        if (string.IsNullOrWhiteSpace(content.MsgType))
+        {
+            content.MsgType = "m.text";
+        }
+    }
+    public MatrixMessageEvent(string roomId, string sender, string body, string msgType = "m.text", List<MatrixEvent>? previousEvents = null)
+        : this(roomId, sender, new MatrixMessage { MsgType = msgType, Body = body }, previousEvents)
+    {
+    }
+}
--- Sparc.Core/Chat/MessageEvent.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Sparc.Core.Chat;
-
-public class MessageEvent : Event
-{
-    public string MsgType { get; set; }
-    public string Body { get; set; }
-}
