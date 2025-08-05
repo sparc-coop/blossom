0257bf625519e2b9bc265ff8e4e5bdc0adf08a83
Fernando Rodrigues <ferd-rodrigues@hotmail.com>
2025-08-05 19:24:05 -0300

    Added Sparc.MCN to use as base for frontend & Merged Room page to Example project

---
+++ Sparc.Blossom.Chat.Example/Users/Contact.cs
@@ -0,0 +1,233 @@
+﻿namespace Sparc.MCN.Users;
+public class Contact
+{
+    public string? UserId { get; set; }
+    public string? Address1 { get; set; }
+    public string? Address2 { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? PostalCode { get; set; }
+    public string? Country { get; set; }
+    public List<Email>? EmailAddresses { get; set; } = new List<Email>();
+    public List<Phone>? PhoneNumbers { get; set; } = new List<Phone>();
+    public string? StateAbbrev => GetStateByName(State ?? string.Empty);
+    public string? AddressString => $"{City}, {StateAbbrev} {PostalCode}";
+    public Contact(string? address1, string? address2, string? city, string? state, string? postalCode, string? country, List<Email>? emails, List<Phone>? phoneNumbers)
+    {
+        Address1 = address1;
+        Address2 = address2;
+        City = city;
+        State = state;
+        PostalCode = postalCode;
+        Country = country;
+        EmailAddresses = emails;
+        PhoneNumbers = phoneNumbers;
+    }
+
+    public string GetStateByName(string name)
+    {
+        switch (name.ToUpper())
+        {
+            case "ALABAMA":
+                return "AL";
+
+            case "ALASKA":
+                return "AK";
+
+            case "AMERICAN SAMOA":
+                return "AS";
+
+            case "ARIZONA":
+                return "AZ";
+
+            case "ARKANSAS":
+                return "AR";
+
+            case "CALIFORNIA":
+                return "CA";
+
+            case "COLORADO":
+                return "CO";
+
+            case "CONNECTICUT":
+                return "CT";
+
+            case "DELAWARE":
+                return "DE";
+
+            case "DISTRICT OF COLUMBIA":
+                return "DC";
+
+            case "FEDERATED STATES OF MICRONESIA":
+                return "FM";
+
+            case "FLORIDA":
+                return "FL";
+
+            case "GEORGIA":
+                return "GA";
+
+            case "GUAM":
+                return "GU";
+
+            case "HAWAII":
+                return "HI";
+
+            case "IDAHO":
+                return "ID";
+
+            case "ILLINOIS":
+                return "IL";
+
+            case "INDIANA":
+                return "IN";
+
+            case "IOWA":
+                return "IA";
+
+            case "KANSAS":
+                return "KS";
+
+            case "KENTUCKY":
+                return "KY";
+
+            case "LOUISIANA":
+                return "LA";
+
+            case "MAINE":
+                return "ME";
+
+            case "MARSHALL ISLANDS":
+                return "MH";
+
+            case "MARYLAND":
+                return "MD";
+
+            case "MASSACHUSETTS":
+                return "MA";
+
+            case "MICHIGAN":
+                return "MI";
+
+            case "MINNESOTA":
+                return "MN";
+
+            case "MISSISSIPPI":
+                return "MS";
+
+            case "MISSOURI":
+                return "MO";
+
+            case "MONTANA":
+                return "MT";
+
+            case "NEBRASKA":
+                return "NE";
+
+            case "NEVADA":
+                return "NV";
+
+            case "NEW HAMPSHIRE":
+                return "NH";
+
+            case "NEW JERSEY":
+                return "NJ";
+
+            case "NEW MEXICO":
+                return "NM";
+
+            case "NEW YORK":
+                return "NY";
+
+            case "NORTH CAROLINA":
+                return "NC";
+
+            case "NORTH DAKOTA":
+                return "ND";
+
+            case "NORTHERN MARIANA ISLANDS":
+                return "MP";
+
+            case "OHIO":
+                return "OH";
+
+            case "OKLAHOMA":
+                return "OK";
+
+            case "OREGON":
+                return "OR";
+
+            case "PALAU":
+                return "PW";
+
+            case "PENNSYLVANIA":
+                return "PA";
+
+            case "PUERTO RICO":
+                return "PR";
+
+            case "RHODE ISLAND":
+                return "RI";
+
+            case "SOUTH CAROLINA":
+                return "SC";
+
+            case "SOUTH DAKOTA":
+                return "SD";
+
+            case "TENNESSEE":
+                return "TN";
+
+            case "TEXAS":
+                return "TX";
+
+            case "UTAH":
+                return "UT";
+
+            case "VERMONT":
+                return "VT";
+
+            case "VIRGIN ISLANDS":
+                return "VI";
+
+            case "VIRGINIA":
+                return "VA";
+
+            case "WASHINGTON":
+                return "WA";
+
+            case "WEST VIRGINIA":
+                return "WV";
+
+            case "WISCONSIN":
+                return "WI";
+
+            case "WYOMING":
+                return "WY";
+        }
+
+        throw new Exception("Not Available");
+    }
+}
+
+public class Email
+{
+    public string? Type { get; set; }
+    public string? Address { get; set; }
+    public Email(string? type, string? address)
+    {
+        Type = type;
+        Address = address;
+    }
+}
+
+public class Phone
+{
+    public string? Type { get; set; }
+    public string? Number { get; set; }
+    public Phone(string? type, string? number)
+    {
+        Type = type;
+        Number = number;
+    }
+}
