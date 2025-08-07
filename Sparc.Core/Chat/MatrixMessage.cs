3289ceceea2afc11681ff06414b15c44927f1bf9
Joseph Young <joseph@josephayoung.com>
2025-08-07 08:03:11 -0500

    Build fix

--- Sparc.Core/Chat/MatrixMessage.cs
+++ Sparc.Core/Chat/MatrixMessage.cs
@@ -4,6 +4,10 @@ public record MatrixMessage(string Body, string MsgType = "m.text");
 public class MatrixMessageEvent(string roomId, string sender, MatrixMessage content, List<MatrixEvent>? previousEvents = null) 
     : MatrixEvent<MatrixMessage>("m.room.message", roomId, sender, content, previousEvents)
 {
+    public MatrixMessageEvent() : this(string.Empty, string.Empty, new MatrixMessage(""))
+    {
+    }
+
     public MatrixMessageEvent(string roomId, string sender, string body, string msgType = "m.text", List<MatrixEvent>? previousEvents = null)
         : this(roomId, sender, new(body, msgType), previousEvents)
     {
