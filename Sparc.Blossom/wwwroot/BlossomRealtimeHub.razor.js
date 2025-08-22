e0e370338253667d1e10e6fe17a670bb96d2049f
Joseph Young <joseph@josephayoung.com>
2025-08-22 07:46:30 -0500

    Fix typescript Dexie build in Blossom

---
+++ Sparc.Blossom/wwwroot/BlossomRealtimeHub.razor.js
@@ -0,0 +1,10 @@
+import '../Data/Dexie/dexie-observable/dexie-observable';
+import '../Data/Dexie/dexie-syncable/dexie-syncable';
+class BlossomSyncProtocol {
+    partialsThreshold;
+    sync(context, url, options, baseRevision, syncedRevision, changes, partial, applyRemoteChanges, onChangesAccepted, onSuccess, onError) {
+        // This should be an adapter for the Blossom realtime hub
+        // tailored after https://github.com/dexie/Dexie.js/blob/master/samples/remote-sync/websocket/WebSocketSyncProtocol.js
+    }
+}
+//# sourceMappingURL=BlossomRealtimeHub.razor.js.map
